50ec564b79927e341d18d9e40131bf46d8069574
m-silver <mattsilver@windowslive.com>
2020-08-28 11:10:18 +1200

    Add meme images and links to imageList

--- src/components/ImageList.jsx
+++ src/components/ImageList.jsx
@@ -2,12 +2,15 @@ import React from 'react'
 
 import {Grid, Image} from 'semantic-ui-react'
 
+import ReturnToHome from './ReturnToHome'
+
 import Don from "../images/don.png"
 import Karel from "../images/karel.png"
 import Emily from "../images/emily.png"
 import Prue from "../images/prue.png"
 
 function ImageList() {
+  
   return (
     <>
       <h1>ImageList</h1>
@@ -17,10 +20,10 @@ function ImageList() {
         <Grid.Row columns={4}>
           <Grid.Column></Grid.Column>
           <Grid.Column>
-            <Image src={Don} />
+            <Image src={Don} href='#/memeBuilder/don' />
           </Grid.Column>
           <Grid.Column>
-            <Image src={Karel} />
+            <Image src={Karel} href='#/memeBuilder/karel' />
           </Grid.Column>
           <Grid.Column></Grid.Column>
         </Grid.Row>
@@ -28,15 +31,17 @@ function ImageList() {
         <Grid.Row columns={4}>
           <Grid.Column></Grid.Column>
           <Grid.Column>
-            <Image src={Emily} />
+            <Image src={Emily} href='#/memeBuilder/emily' />
           </Grid.Column>
           <Grid.Column>
-            <Image src={Prue} />
+            <Image src={Prue} href='#/memeBuilder/prue' />
           </Grid.Column>
           <Grid.Column></Grid.Column>
         </Grid.Row>
 
       </Grid>
+
+      <ReturnToHome />
     </>
   )
 }
